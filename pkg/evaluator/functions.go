a04410cee7a372f7774aac655bb66ede543d7fee
Maksym Trofimenko <maksym@container-registry.com>
2024-09-08 22:00:13 +0100

    add b64encode and b64decode functions

--- pkg/evaluator/functions.go
+++ pkg/evaluator/functions.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"encoding/base64"
 	"fmt"
 	"github.com/spyzhov/ajson"
 	"time"
@@ -105,5 +106,23 @@ func init() {
 			val = "unknown"
 		}
 		return ajson.StringNode("string", val), nil
+	})
+
+	ajson.AddFunction("b64encode", func(node *ajson.Node) (result *ajson.Node, err error) {
+		if !node.IsString() {
+			return nil, fmt.Errorf("not a string")
+		}
+		return ajson.StringNode("b64encode", base64.StdEncoding.EncodeToString([]byte(node.MustString()))), nil
+	})
+
+	ajson.AddFunction("b64decode", func(node *ajson.Node) (result *ajson.Node, err error) {
+		if !node.IsString() {
+			return nil, fmt.Errorf("not a string")
+		}
+		decodedStrAsByteSlice, err := base64.StdEncoding.DecodeString(node.String())
+		if err != nil {
+			return nil, fmt.Errorf("error decoding base64 string: %v", err)
+		}
+		return ajson.StringNode("b64decode", string(decodedStrAsByteSlice)), nil
 	})
 }
