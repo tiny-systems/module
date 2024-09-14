73244988a55f0ef91048ec4a28c51aaa43637e87
Maksym Trofimenko <maksym@container-registry.com>
2024-09-14 13:16:13 +0100

    add more tests bugfix

--- pkg/schema/json_test.go
+++ pkg/schema/json_test.go
@@ -1,67 +1,324 @@
 package schema
 
 import (
+	"fmt"
 	"github.com/google/go-cmp/cmp"
+	"github.com/spyzhov/ajson"
 	"github.com/swaggest/jsonschema-go"
-	"github.com/tiny-systems/module/module"
 	"testing"
 )
 
 type ModifyContext any
+type Modify2Context any
 
 type ModifyInMessage struct {
-	Context ModifyContext `json:"context" configurable:"true" required:"true" title:"Context" description:"Arbitrary message to be modified"`
+	Context  ModifyContext  `json:"context" configurable:"true" required:"true" title:"Context" description:"Arbitrary message to be modified"`
+	Context2 Modify2Context `json:"context2"`
 }
 
-type ModifyOutMessage struct {
-	Context ModifyContext `json:"context"`
-}
-
+// TestSchemaConsistency test that we generate schema same way
 func TestSchemaConsistency(t *testing.T) {
-
-	sharedConfigurableSchemaDefinitions := make(map[string]jsonschema.Schema)
-
-	ports := []module.Port{
-		{
-			Configuration: ModifyInMessage{},
-		},
-		{
-			Configuration: ModifyOutMessage{},
-		},
+	ports := []interface{}{
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
+		ModifyInMessage{},
 	}
 
-	for _, p := range ports {
-		_, err := CreateSchema(p.Configuration)
+	var prevSchemaData []byte
+
+	for i, port := range ports {
+		schema, err := CreateSchema(port)
 		if err != nil {
 			t.Fatalf("unexpected error: %v", err)
 		}
+		schemaData, err := schema.MarshalJSON()
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		i++
+		if i <= 1 {
+			// do not compare on a first loop
+			prevSchemaData = schemaData
+			continue
+		}
+
+		diff := cmp.Diff(string(schemaData), string(prevSchemaData))
+		if diff != "" {
+			t.Fatalf("%d schema not equal prev schema: %s", i, diff)
+		}
+		prevSchemaData = schemaData
 	}
+}
 
-	t.Logf("shared configurable defs size: %d", len(sharedConfigurableSchemaDefinitions))
-
-	schema, err := CreateSchema(ports[0].Configuration)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
+func TestCollectDefinitions(t *testing.T) {
+	type args struct {
+		s        []byte
+		confDefs map[string]*ajson.Node
 	}
-	schemaData, err := schema.MarshalJSON()
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
+	tests := []struct {
+		name     string
+		args     args
+		wantErr  bool
+		checkMap func(map[string]*ajson.Node) error
+	}{
+		{
+			name: "invalid empty json, should error",
+			args: args{
+				s:        []byte(``),
+				confDefs: map[string]*ajson.Node{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid empty json, should error",
+			args: args{
+				s:        []byte(``),
+				confDefs: map[string]*ajson.Node{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "no $defs, should error",
+			args: args{
+				s:        []byte(`{}`),
+				confDefs: map[string]*ajson.Node{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "no configurable of shared defs, no error",
+			args: args{
+				s: []byte(`{
+        "$defs": {
+          "Test":{"type":"string"},
+          "Test":{"type":"number"}
+        }
+        }`),
+				confDefs: map[string]*ajson.Node{},
+			},
+			wantErr: false,
+			checkMap: func(m map[string]*ajson.Node) error {
+				if len(m) > 0 {
+					return fmt.Errorf("expected no configurable or shared defs")
+				}
+				return nil
+			},
+		},
+		{
+			name: "1 configurable and 1 shared def, no error",
+			args: args{
+				s: []byte(`{
+        "$defs": {
+          "Test":{"type":"string", "configurable": true},
+          "Test2":{"type":"number", "shared": true}
+        }
+        }`),
+				confDefs: map[string]*ajson.Node{},
+			},
+			wantErr: false,
+			checkMap: func(m map[string]*ajson.Node) error {
+				if len(m) != 2 {
+					return fmt.Errorf("expected 2 configurable/shared defs: found %d", len(m))
+				}
+				return nil
+			},
+		},
+
+		{
+			name: "check configurable definition type, no error",
+			args: args{
+				s: []byte(`{
+        "$defs": {
+          "Test":{"type":"specialtype", "configurable": true}
+          }
+        }`),
+				confDefs: map[string]*ajson.Node{},
+			},
+			wantErr: false,
+			checkMap: func(m map[string]*ajson.Node) error {
+				def, ok := m["Test"]
+				if !ok {
+					return fmt.Errorf("expected Test to exist")
+				}
+				if st, _ := getStr("type", def); st != "specialtype" {
+					return fmt.Errorf("expected type")
+				}
+				return nil
+			},
+		},
 	}
-
-	schema2, err := CreateSchema(ports[0].Configuration)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := CollectDefinitions(tt.args.s, tt.args.confDefs); (err != nil) != tt.wantErr {
+				t.Errorf("CollectDefinitions() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.checkMap == nil {
+				return
+			}
+			if err := tt.checkMap(tt.args.confDefs); err != nil {
+				t.Errorf("checkMap error = %v", err)
+			}
+		})
 	}
+}
 
-	schema2Data, err := schema2.MarshalJSON()
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
+type routeName struct {
+	Value   string   `json:"value"`
+	Options []string `json:"options"`
+}
+
+func (r routeName) JSONSchema() (jsonschema.Schema, error) {
+	name := jsonschema.Schema{}
+	name.AddType(jsonschema.String)
+	name.WithTitle("Route")
+	name.WithDefault(r.Value)
+	name.WithExtraPropertiesItem("shared", true)
+	enums := make([]interface{}, len(r.Options))
+	for k, v := range r.Options {
+		enums[k] = v
 	}
+	name.WithEnum(enums...)
+	return name, nil
+}
 
-	t.Log(string(schemaData))
+func TestCreateSchema(t *testing.T) {
+	// using different objects to generate their JSON schemas
 
-	diff := cmp.Diff(schemaData, schema2Data)
-	if diff != "" {
-		t.Errorf("not equal: %s", diff)
+	tests := []struct {
+		name    string
+		getObj  func() interface{}
+		want    *jsonschema.Schema
+		wantErr bool
+	}{
+		//
+		{
+			name: "nil object",
+			getObj: func() interface{} {
+				return nil
+			},
+			want: (&jsonschema.Schema{}).WithExtraPropertiesItem("$defs", map[string]jsonschema.Schema{}),
+		},
+		//
+		{
+			name: "empty struct object",
+			getObj: func() interface{} {
+				return struct {
+				}{}
+			},
+			want: (&jsonschema.Schema{}).WithType(jsonschema.Object.Type()).WithExtraPropertiesItem("$defs", map[string]jsonschema.Schema{}),
+		},
+		//
+		{
+			name: "check scalar tags presented as extra properties, any type props are definitions",
+			getObj: func() interface{} {
+
+				type ModifyContext any // no type
+				type ModifyInMessage struct {
+					Context ModifyContext `json:"context" configurable:"true" required:"true" title:"Context" description:"Arbitrary message to be modified"`
+				}
+				return ModifyInMessage{}
+			},
+			//
+			want: (&jsonschema.Schema{}).
+				WithRef("#/$defs/Modifyinmessage"). // on a root level we only have reference to the definition
+				WithExtraPropertiesItem("$defs", map[string]jsonschema.Schema{
+					///
+					"Modifycontext": *((&jsonschema.Schema{}).WithTitle("Context").WithDescription("Arbitrary message to be modified")).WithExtraProperties(map[string]interface{}{
+						"configurable":  true,
+						"path":          "$.context", // JSON path of this definition related to the root object
+						"propertyOrder": 1,           // first property of it's level
+					}),
+					///
+					"Modifyinmessage": *((&jsonschema.Schema{}).WithType(jsonschema.Object.Type()).WithRequired("context").
+						WithProperties(map[string]jsonschema.SchemaOrBool{
+							"context": (&jsonschema.Schema{}).WithRef("#/$defs/Modifycontext").WithExtraProperties(map[string]interface{}{
+								"propertyOrder": 1, // first property of it's level
+							}).ToSchemaOrBool(),
+						}).WithExtraPropertiesItem("path", "$")), // root level
+					///
+				}),
+		},
+		//
+		{
+			name: "array tag enumTitles",
+			getObj: func() interface{} {
+
+				type Request struct {
+					Method string `json:"method" title:"Method" enum:"get,post,patch,put,delete" enumTitles:"GET,POST,PATCH,PUT,DELETE"`
+				}
+				return Request{}
+			},
+			//
+			want: (&jsonschema.Schema{}).
+				WithRef("#/$defs/Request"). // on a root level we only have reference to the definition
+				WithExtraPropertiesItem("$defs", map[string]jsonschema.Schema{
+					///
+					"Request": *((&jsonschema.Schema{}).WithType(jsonschema.Object.Type()).WithProperties(map[string]jsonschema.SchemaOrBool{
+						"method": (&jsonschema.Schema{}).WithType(jsonschema.String.Type()).WithTitle("Method").WithEnum("get", "post", "patch", "put", "delete").WithExtraProperties(map[string]interface{}{
+							"enumTitles":    []string{"GET", "POST", "PATCH", "PUT", "DELETE"},
+							"propertyOrder": 1,
+						}).ToSchemaOrBool(),
+					}).
+						WithExtraProperties(map[string]interface{}{
+							"path": "$", // Request is root
+						})),
+				}),
+		},
+		{
+			name: "test schema exposer",
+			getObj: func() interface{} {
+				return routeName{
+					Value:   "test",
+					Options: []string{"test", "none", ""},
+				}
+			},
+			//
+			want: (&jsonschema.Schema{}).
+				WithRef("#/$defs/Routename"). // on a root level we only have reference to the definition
+				WithExtraPropertiesItem("$defs", map[string]jsonschema.Schema{
+					///
+					"Routename": *((&jsonschema.Schema{}).WithType(jsonschema.String.Type()). // string
+															WithTitle("Route").
+															WithDefault("test").
+															WithEnum("test", "none", "").
+															WithExtraProperties(map[string]interface{}{
+							"propertyOrder": 1,
+						}).
+						WithExtraProperties(map[string]interface{}{
+							"path":   "$", // Request is root
+							"shared": true,
+						})),
+				}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := CreateSchema(tt.getObj())
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CreateSchema() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			g, _ := got.MarshalJSON()
+			w, _ := tt.want.MarshalJSON()
+
+			if d := cmp.Diff(string(g), string(w)); d != "" {
+				t.Errorf("expected schema mismatch: %s; expected: %s; got: %s", d, string(w), string(g))
+			}
+		})
 	}
 }
---
+++ pkg/schema/schema_test.go
@@ -0,0 +1,131 @@
+package schema
+
+import (
+	"github.com/spyzhov/ajson"
+	"reflect"
+	"testing"
+)
+
+func TestUpdateWithDefinitions(t *testing.T) {
+	type args struct {
+		realSchema                  []byte
+		configurableDefinitionNodes map[string]*ajson.Node
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{
+			name: "empty schema should cause error",
+			args: args{
+				realSchema:                  []byte(``),
+				configurableDefinitionNodes: map[string]*ajson.Node{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "schema with no $defs should cause error",
+			args: args{
+				realSchema:                  []byte(`{}`),
+				configurableDefinitionNodes: map[string]*ajson.Node{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "empty defs returns as it is, no error",
+			args: args{
+				realSchema:                  []byte(`{"$defs":{}}`),
+				configurableDefinitionNodes: map[string]*ajson.Node{},
+			},
+			wantErr: false,
+			want:    []byte(`{"$defs":{}}`),
+		},
+		{
+			name: "updates SomeDefinition from map, keep non configurable false because original schema has not it defined, no error",
+			args: args{
+				// real schema has it as simple string type with no format
+				realSchema: []byte(`{"$defs":{"SomeDefinition": {"type": "string"}}}`),
+				configurableDefinitionNodes: map[string]*ajson.Node{
+					"SomeDefinition": ajson.Must(ajson.Unmarshal([]byte(`{"type":"string", "format":"textarea"}`))),
+				},
+			},
+			wantErr: false,
+			// now we have explicitly configurable == false
+			// we see format from defintions
+			want: []byte(`{"$defs":{"SomeDefinition":{"configurable":false,"format":"textarea","type":"string"}}}`),
+		},
+		{
+			name: "updates SomeDefinition from map, keep non configurable true because original schema has it that way, no error",
+			args: args{
+				// real schema has it as simple string type with no format
+				realSchema: []byte(`{"$defs":{"SomeDefinition": {"type": "string","configurable":false}}}`),
+				configurableDefinitionNodes: map[string]*ajson.Node{
+					// ignore configurable flag from def
+					"SomeDefinition": ajson.Must(ajson.Unmarshal([]byte(`{"type":"string", "format":"textarea", "configurable":true}`))),
+				},
+			},
+			wantErr: false,
+			// now we have explicitly configurable == false
+			// we see format from definitions
+			want: []byte(`{"$defs":{"SomeDefinition":{"configurable":false,"format":"textarea","type":"string"}}}`),
+		},
+		{
+			name: "updates SomeDefinition from map, preserves configurable and path, no error",
+			args: args{
+				// real schema has it as simple string type with no format
+				realSchema: []byte(`{"$defs":{"SomeDefinition": {"type": "string","configurable":true, "path": "random"}}}`),
+				configurableDefinitionNodes: map[string]*ajson.Node{
+					// ignore configurable flag from def
+					"SomeDefinition": ajson.Must(ajson.Unmarshal([]byte(`{"type": "string", "format": "textarea", "configurable": false}`))),
+				},
+			},
+			wantErr: false,
+			// now we have explicitly configurable == false
+			// we see format from definitions
+			want: []byte(`{"$defs":{"SomeDefinition":{"configurable":true,"format":"textarea","path":"random","type":"string"}}}`),
+		},
+		{
+			name: "updates SomeDefinition from map, preserves configurable and path, drops some param, no error",
+			args: args{
+				// real schema has it as simple string type with no format
+				realSchema: []byte(`{"$defs":{"SomeDefinition": {"type": "string","configurable":true, "path": "random", "some": "param"}}}`),
+				configurableDefinitionNodes: map[string]*ajson.Node{
+					// ignore configurable flag from def
+					"SomeDefinition": ajson.Must(ajson.Unmarshal([]byte(`{"type": "string", "format": "textarea", "configurable": false}`))),
+				},
+			},
+			wantErr: false,
+			// now we have explicitly configurable == false
+			// we see format from definitions
+			want: []byte(`{"$defs":{"SomeDefinition":{"configurable":true,"format":"textarea","path":"random","type":"string"}}}`),
+		},
+		{
+			name: "updates nothing, no definition match, no error",
+			args: args{
+				// real schema has it as simple string type with no format
+				realSchema: []byte(`{"$defs":{"SunDefinition": {"type": "string"}}}`),
+				configurableDefinitionNodes: map[string]*ajson.Node{
+					"SomeDefinition": ajson.Must(ajson.Unmarshal([]byte(`{"type": "string", "format": "textarea"}`))),
+				},
+			},
+			wantErr: false,
+			// now we have explicitly configurable == false
+			// we see format from definitions
+			want: []byte(`{"$defs":{"SunDefinition": {"type": "string"}}}`),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := UpdateWithDefinitions(tt.args.realSchema, tt.args.configurableDefinitionNodes)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("UpdateWithConfigurableDefinitions() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("UpdateWithConfigurableDefinitions() got = %s, want %s", got, tt.want)
+			}
+		})
+	}
+}
