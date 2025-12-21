7a31384c6096917e0d92a668a20e01cfaa698f4a
Tiny Systems <dev@tinysystems.io>
2025-12-21 21:45:00 +0000

    adds more logs

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -98,10 +98,22 @@ func (s *Schedule) Install(component module.Component) error {
 // Handle could be external and synchronous
 // @todo use retry with backoff here
 func (s *Schedule) Handle(ctx context.Context, msg *runner.Msg) (any, error) {
-
 	nodeName, port := utils.ParseFullPortName(msg.To)
+
+	s.log.Info("scheduler handle: received message",
+		"to", msg.To,
+		"from", msg.From,
+		"node", nodeName,
+		"port", port,
+		"edgeID", msg.EdgeID,
+		"dataSize", len(msg.Data),
+	)
+
 	if port == v1alpha1.ReconcilePort {
 		// system port; do nothing
+		s.log.Info("scheduler handle: skipping reconcile port",
+			"node", nodeName,
+		)
 		return nil, nil
 	}
 
@@ -111,29 +123,76 @@ func (s *Schedule) Handle(ctx context.Context, msg *runner.Msg) (any, error) {
 		// instance is not registered currently, or it's port is not yet available (settings did not enable it yet?)
 		// maybe reconcile call did not register it yet
 		// sleep and try again
+		s.log.Info("scheduler handle: instance not ready, delegating to outside handler",
+			"node", nodeName,
+			"port", port,
+			"instanceExists", ok,
+		)
+
 		t := time.NewTimer(time.Millisecond)
 		defer t.Stop()
 
 		select {
 		// what ever happens first
 		case <-ctx.Done():
+			s.log.Info("scheduler handle: context cancelled while waiting",
+				"node", nodeName,
+				"port", port,
+			)
 			return nil, nil
 		case <-t.C:
 			return s.msgHandler(ctx, msg)
 		}
 	}
+
+	s.log.Info("scheduler handle: routing to local instance",
+		"node", nodeName,
+		"port", port,
+	)
 	return s.sendMsg(ctx, instance, msg)
 }
 
 func (s *Schedule) sendMsg(ctx context.Context, instance *runner.Runner, msg *runner.Msg) (any, error) {
-	return instance.MsgHandler(ctx, msg, func(outCtx context.Context, outMsg *runner.Msg) (any, error) {
+	s.log.Info("scheduler send msg: invoking instance handler",
+		"to", msg.To,
+		"from", msg.From,
+		"edgeID", msg.EdgeID,
+	)
+
+	res, err := instance.MsgHandler(ctx, msg, func(outCtx context.Context, outMsg *runner.Msg) (any, error) {
 		// output
 		_, port := utils.ParseFullPortName(outMsg.To)
 		if port == "" {
+			s.log.Error(fmt.Errorf("empty port in handle's output"), "scheduler send msg: invalid output message",
+				"to", outMsg.To,
+				"from", outMsg.From,
+			)
 			return nil, fmt.Errorf("empty port in handle's output")
 		}
+
+		s.log.Info("scheduler send msg: forwarding output to handler",
+			"to", outMsg.To,
+			"from", outMsg.From,
+			"edgeID", outMsg.EdgeID,
+			"dataSize", len(outMsg.Data),
+		)
 		return s.msgHandler(outCtx, outMsg)
 	})
+
+	if err != nil {
+		s.log.Error(err, "scheduler send msg: instance handler failed",
+			"to", msg.To,
+			"from", msg.From,
+			"edgeID", msg.EdgeID,
+		)
+	} else {
+		s.log.Info("scheduler send msg: completed",
+			"to", msg.To,
+			"hasResponse", res != nil,
+		)
+	}
+
+	return res, err
 }
 
 func (s *Schedule) Destroy(name string) error {
@@ -148,19 +207,30 @@ func (s *Schedule) Destroy(name string) error {
 // Update updates node instance or creates one based on tinyNode crd
 // updates status resource based on outcome of Update
 func (s *Schedule) Update(ctx context.Context, node *v1alpha1.TinyNode) error {
+	s.log.Info("scheduler update: updating node instance",
+		"node", node.Name,
+		"component", node.Spec.Component,
+		"edgeCount", len(node.Spec.Edges),
+	)
 
 	cmp, ok := s.componentsMap.Get(node.Spec.Component)
 	if !ok {
 		// not recoverable error
+		s.log.Error(fmt.Errorf("component not registered"), "scheduler update: component not found",
+			"node", node.Name,
+			"component", node.Spec.Component,
+		)
 		return fmt.Errorf("component %s is not registered", node.Spec.Component)
 	}
 
 	// get or create
 
+	isNew := false
 	runnerInstance := s.instancesMap.Upsert(node.Name, nil, func(exist bool, runnerInstance *runner.Runner, _ *runner.Runner) *runner.Runner {
 		if exist {
 			return runnerInstance
 		}
+		isNew = true
 		// new instance
 		return runner.NewRunner(cmp.Instance()).
 			SetNode(*node).
@@ -170,6 +240,17 @@ func (s *Schedule) Update(ctx context.Context, node *v1alpha1.TinyNode) error {
 			SetTracker(s.tracker).
 			SetMeter(s.meter)
 	})
+
+	if isNew {
+		s.log.Info("scheduler update: created new runner instance",
+			"node", node.Name,
+			"component", node.Spec.Component,
+		)
+	} else {
+		s.log.Info("scheduler update: updating existing runner instance",
+			"node", node.Name,
+		)
+	}
 
 	// backup if new not will node work to avoid disruption to working system due to faulty settings
 	var nodeBackup = runnerInstance.Node()
@@ -184,6 +265,9 @@ func (s *Schedule) Update(ctx context.Context, node *v1alpha1.TinyNode) error {
 			return
 		}
 		// instance failed with new version of node, backup
+		s.log.Error(err, "scheduler update: rolling back to previous node state",
+			"node", node.Name,
+		)
 
 		runnerInstance.SetNode(nodeBackup)
 		node.Status.Status = err.Error()
@@ -222,12 +306,24 @@ func (s *Schedule) Update(ctx context.Context, node *v1alpha1.TinyNode) error {
 	}
 
 	// sendMsg signal to the settings ports with no data so node will apply own configs (own means "from" is empty for those configs)
+	s.log.Info("scheduler update: sending settings message",
+		"node", node.Name,
+		"settingsPort", v1alpha1.SettingsPort,
+	)
+
 	if _, err = s.sendMsg(ctx, runnerInstance, &runner.Msg{
 		To:   utils.GetPortFullName(node.Name, v1alpha1.SettingsPort),
 		Data: []byte("{}"), // no external data sent to port, rely solely on a node's port config
 	}); err != nil {
+		s.log.Error(err, "scheduler update: failed to send settings message",
+			"node", node.Name,
+		)
 		return nil
 	}
+
+	s.log.Info("scheduler update: completed successfully",
+		"node", node.Name,
+	)
 
 	// do we need to rebuild status in case of rollback?
 	return runnerInstance.ReadStatus(&node.Status)
